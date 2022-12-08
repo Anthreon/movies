cceacf9dd90a39c3ae394e4a043b4c9ca4f93e6f
Anthreon <michal.rykalsky@gmail.com>
2022-12-08 12:42:17 +0100

    feature: Added fetching movies logic

--- src/pages/EntryPage.tsx
+++ src/pages/EntryPage.tsx
@@ -1,15 +1,72 @@
-import { FC } from "react";
+import { FC, useContext, useEffect } from "react";
 import SearchInput from "../components/SearchInput";
 import Styles from "./EntryPage.module.css";
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import axios from "axios";
+import { SearchContext } from "../store/search-context";
+const API_URL = "http://omdbapi.com/?apikey=aa5c3014&r=json&type=movie";
+
+interface MovieDetail {
+  image: string;
+  title: string;
+  type: string;
+  year: string;
+  id: string;
+}
 
 const EntryPage: FC = () => {
+  const searchCtx = useContext(SearchContext);
+  const validString: boolean = searchCtx.searchedInput.length > 2;
+  console.log(validString);
+
+  const {
+    data,
+    isLoading,
+    isError,
+  }: { data: MovieDetail[] | undefined; isLoading: boolean; isError: boolean } =
+    useQuery(
+      ["movies", searchCtx.searchedInput],
+      fetchMovies.bind(this, `&s=${searchCtx.searchedInput}`),
+      {
+        enabled: validString,
+      }
+    );
+
+  async function fetchMovies(searchMovieInput: string): Promise<MovieDetail[]> {
+    const { data } = await axios.get(`${API_URL + searchMovieInput}`);
+
+    const mappedMovies: MovieDetail[] = data.Search.map((movie: any) => {
+      return {
+        image: movie.Poster,
+        title: movie.Title,
+        type: movie.Type,
+        year: movie.Year,
+        id: movie.imdbID,
+      };
+    });
+
+    return mappedMovies;
+  }
+
+  //   if (isLoading) {
+  //     return <div>Loading...</div>;
+  //   }
+  //   if (isError) {
+  //     return <div>Error! {isError}</div>;
+  //   }
+
   return (
     <div>
       <header className={Styles.header}>
         <SearchInput></SearchInput>
       </header>
       <Link to="favourites">Navigate to favourites</Link>
+      {!isLoading && !isError
+        ? data?.map((movie: any, index: number) => {
+            return <li key={index}>{movie.title}</li>;
+          })
+        : null}
     </div>
   );
 };
--- src/routing/MainRoutes.tsx
+++ src/routing/MainRoutes.tsx
@@ -3,17 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import EntryPage from "../pages/EntryPage";
 import FavouriteMovies from "../pages/FavouriteMovies";
 import SearchContextProvider from "../store/search-context";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+
+const queryClient = new QueryClient();
 
 const MainRoutes: FC = () => {
   return (
-    <SearchContextProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<EntryPage />} />
-          <Route path="favourites" element={<FavouriteMovies />} />
-        </Routes>
-      </BrowserRouter>
-    </SearchContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <SearchContextProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<EntryPage />} />
+            <Route path="favourites" element={<FavouriteMovies />} />
+          </Routes>
+        </BrowserRouter>
+      </SearchContextProvider>
+    </QueryClientProvider>
   );
 };
 export default MainRoutes;
