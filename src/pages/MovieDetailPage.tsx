26eb44a36b2443d2ce0960cb7212d8d0594fdc13
Anthreon <michal.rykalsky@gmail.com>
2022-12-09 17:39:19 +0100

    feature: Added styling to movie detail page

--- src/pages/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage.tsx
@@ -18,35 +18,37 @@ const MovieDetailPage: FC = () => {
   }, []);
 
   return (
-    <main>
-      <div className={Styles.container}>
-        <div>
-          <section className={Styles.imgWrapper}>
-            <img
-              className={Styles.img}
-              src={movieData?.poster ? movieData.poster : MoviePlaceholder}
-              alt="Movie image"
-            />
-            <div>
-              <aside className={Styles.movieDetails}>
-                <ul className={Styles.movieDataList}>
-                  <li>Title: {movieData?.title}</li>
-                  <li>Actors: {movieData?.actors}</li>
-                  <li>Awards: {movieData?.awards}</li>
-                  <li>Rating: {movieData?.imdbrating}</li>
-                  <li>Director: {movieData?.director}</li>
-                  <li>Genre: {movieData?.genre}</li>
-                  <li>Language: {movieData?.language}</li>
-                  <li>Year of creation: {movieData?.year}</li>
-                  <li>Writers: {movieData?.writer}</li>
-                  <li>Story of film: {movieData?.plot}</li>
-                </ul>
-              </aside>
-            </div>
-          </section>
+    <div className={Styles.pageContainer}>
+      <main>
+        <div className={Styles.container}>
+          <div>
+            <section className={Styles.imgWrapper}>
+              <img
+                className={Styles.img}
+                src={movieData?.poster ? movieData.poster : MoviePlaceholder}
+                alt="Movie image"
+              />
+              <div>
+                <aside className={Styles.movieDetails}>
+                  <ul className={Styles.movieDataList}>
+                    <li>Title: {movieData?.title}</li>
+                    <li>Actors: {movieData?.actors}</li>
+                    <li>Awards: {movieData?.awards}</li>
+                    <li>Rating: {movieData?.imdbrating}</li>
+                    <li>Director: {movieData?.director}</li>
+                    <li>Genre: {movieData?.genre}</li>
+                    <li>Language: {movieData?.language}</li>
+                    <li>Year of creation: {movieData?.year}</li>
+                    <li>Writers: {movieData?.writer}</li>
+                    <li>Story of film: {movieData?.plot}</li>
+                  </ul>
+                </aside>
+              </div>
+            </section>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 };
 
