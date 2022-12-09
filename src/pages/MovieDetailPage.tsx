26aa55961b680d0cc6b3b964cfcdbb6e347833e8
Anthreon <michal.rykalsky@gmail.com>
2022-12-09 17:05:07 +0100

    feature: Added default style for movie detail page

--- src/pages/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage.tsx
@@ -1,18 +1,53 @@
-import { FC, useEffect } from "react";
+import { FC, ReactNode, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Styles from "./MovieDetailPage.module.css";
-
-interface MovieDetailPageProps {
-  movieId: string;
-}
-
+import { DetailsAboutMovie, fetchMovieDetailPage } from "../util/http";
+import MoviePlaceholder from "../assets/MoviePlaceholder.jpg";
 const MovieDetailPage: FC = () => {
+  const [movieData, setMovieData] = useState<DetailsAboutMovie | undefined>();
   const location = useLocation();
   const movieId: string = location.state;
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    (async () => {
+      const movieDetails: DetailsAboutMovie = await fetchMovieDetailPage(
+        movieId
+      );
+      setMovieData(movieDetails);
+    })();
+  }, []);
 
-  return <p>asdads {movieId}</p>;
+  return (
+    <main>
+      <div className={Styles.container}>
+        <div>
+          <section className={Styles.imgWrapper}>
+            <img
+              className={Styles.img}
+              src={movieData?.poster ? movieData.poster : MoviePlaceholder}
+              alt="Movie image"
+            />
+            <div>
+              <aside className={Styles.movieDetails}>
+                <ul className={Styles.movieDataList}>
+                  <li>Title: {movieData?.title}</li>
+                  <li>Actors: {movieData?.actors}</li>
+                  <li>Awards: {movieData?.awards}</li>
+                  <li>Rating: {movieData?.imdbrating}</li>
+                  <li>Director: {movieData?.director}</li>
+                  <li>Genre: {movieData?.genre}</li>
+                  <li>Language: {movieData?.language}</li>
+                  <li>Year of creation: {movieData?.year}</li>
+                  <li>Writers: {movieData?.writer}</li>
+                  <li>Story of film: {movieData?.plot}</li>
+                </ul>
+              </aside>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default MovieDetailPage;
---
+++ src/util/http.tsx
@@ -0,0 +1,47 @@
+import axios from "axios";
+
+export interface DetailsAboutMovie {
+  actors: string;
+  awards: string;
+  boxoffice: string;
+  country: string;
+  director: string;
+  dvd: string;
+  genre: string;
+  imdbid: string;
+  imdbrating: string;
+  imdbvotes: string;
+  language: string;
+  metascore: string;
+  plot: string;
+  poster: string;
+  production: string;
+  rated: string;
+  ratings?: [{ Source: string; Value: string }];
+  released: string;
+  response: string;
+  runtime: string;
+  title: string;
+  type: string;
+  website: string;
+  writer: string;
+  year: string;
+}
+
+const API_DETAIL_URL = "http://omdbapi.com/?apikey=aa5c3014&r=json&type=movie";
+
+function lowerObjectKeys(obj: any) {
+  return Object.keys(obj).reduce((accumulator: any, key: any) => {
+    accumulator[key.toLowerCase()] = obj[key];
+    return accumulator;
+  }, {});
+}
+
+export const fetchMovieDetailPage = async (pageId: string) => {
+  const { data } = await axios.get(`${API_DETAIL_URL}&i=${pageId}`);
+  const movie: DetailsAboutMovie = lowerObjectKeys(data);
+  delete movie.ratings;
+  console.log(movie);
+
+  return movie;
+};
