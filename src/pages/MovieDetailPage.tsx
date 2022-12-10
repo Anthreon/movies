4d8b571200d984362167c79171320dd557afbcdd
Anthreon <michal.rykalsky@gmail.com>
2022-12-10 08:57:48 +0100

    feature: Added loading spinner to page

--- src/pages/MovieDetailPage.tsx
+++ src/pages/MovieDetailPage.tsx
@@ -4,8 +4,10 @@ import Styles from "./MovieDetailPage.module.css";
 import { DetailsAboutMovie, fetchMovieDetailPage } from "../util/http";
 import MoviePlaceholder from "../assets/MoviePlaceholder.jpg";
 import { Button } from "@mui/material";
+import BackDropSpinner from "../components/BackDropSpinner";
 const MovieDetailPage: FC = () => {
   const [movieData, setMovieData] = useState<DetailsAboutMovie | undefined>();
+  const [loading, setLoading] = useState<boolean>(true);
   const location = useLocation();
   const navigate = useNavigate();
   const movieId: string = location.state;
@@ -20,49 +22,54 @@ const MovieDetailPage: FC = () => {
         movieId
       );
       setMovieData(movieDetails);
+      setLoading(false);
     })();
   }, []);
 
   return (
-    <div className={Styles.pageContainer}>
-      <Button
-        className={Styles.backButton}
-        onClick={navigateBack}
-        variant="contained"
-        disableElevation
-      >
-        Back
-      </Button>
-      <main>
-        <div className={Styles.container}>
-          <div>
-            <section className={Styles.imgWrapper}>
-              <img
-                className={Styles.img}
-                src={movieData?.poster ? movieData.poster : MoviePlaceholder}
-                alt="Movie image"
-              />
-              <div>
-                <aside className={Styles.movieDetails}>
-                  <ul className={Styles.movieDataList}>
-                    <li>Title: {movieData?.title}</li>
-                    <li>Actors: {movieData?.actors}</li>
-                    <li>Awards: {movieData?.awards}</li>
-                    <li>Rating: {movieData?.imdbrating}</li>
-                    <li>Director: {movieData?.director}</li>
-                    <li>Genre: {movieData?.genre}</li>
-                    <li>Language: {movieData?.language}</li>
-                    <li>Year of creation: {movieData?.year}</li>
-                    <li>Writers: {movieData?.writer}</li>
-                    <li>Story of film: {movieData?.plot}</li>
-                  </ul>
-                </aside>
-              </div>
-            </section>
+    <>
+      {loading && <BackDropSpinner></BackDropSpinner>}
+
+      <div className={Styles.pageContainer}>
+        <Button
+          className={Styles.backButton}
+          onClick={navigateBack}
+          variant="contained"
+          disableElevation
+        >
+          Back
+        </Button>
+        <main>
+          <div className={Styles.container}>
+            <div>
+              <section className={Styles.imgWrapper}>
+                <img
+                  className={Styles.img}
+                  src={movieData?.poster ? movieData.poster : MoviePlaceholder}
+                  alt="Movie image"
+                />
+                <div>
+                  <aside className={Styles.movieDetails}>
+                    <ul className={Styles.movieDataList}>
+                      <li>Title: {movieData?.title}</li>
+                      <li>Actors: {movieData?.actors}</li>
+                      <li>Awards: {movieData?.awards}</li>
+                      <li>Rating: {movieData?.imdbrating}</li>
+                      <li>Director: {movieData?.director}</li>
+                      <li>Genre: {movieData?.genre}</li>
+                      <li>Language: {movieData?.language}</li>
+                      <li>Year of creation: {movieData?.year}</li>
+                      <li>Writers: {movieData?.writer}</li>
+                      <li>Story of film: {movieData?.plot}</li>
+                    </ul>
+                  </aside>
+                </div>
+              </section>
+            </div>
           </div>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </>
   );
 };
 
